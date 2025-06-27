2b1d903f134dfc11a4f307462f2718ae60194f27
dankozz1t <alexdankoxxl@gmail.com>
2025-06-27 22:13:32 +0300

    feat(bundle): optimization, analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,67 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/styles/_variables.scss";',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const isDevelopment = mode === 'development';
+  const isDebug = env.VITE_IS_DEBUG === 'true';
+
+  return {
+    plugins: [
+      vue(),
+      isDebug &&
+        visualizer({
+          filename: 'dist/bundle-report.html',
+          open: false,
+          gzipSize: true,
+          brotliSize: true,
+        }),
+    ],
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+
+    server: {
+      port: 3000,
+    },
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/styles/_variables.scss";',
+        },
+      },
+    },
+
+    build: {
+      sourcemap: isDevelopment ? 'inline' : true,
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['vue', 'vue-router', 'pinia'],
+            vuetify: ['vuetify'],
+          },
+        },
+      },
+
+      minify: isDevelopment ? false : 'terser',
+      terserOptions: isDevelopment
+        ? {}
+        : {
+            compress: {
+              drop_console: true,
+              drop_debugger: true,
+            },
+          },
+
+      target: 'esnext',
+    },
+  };
 });
